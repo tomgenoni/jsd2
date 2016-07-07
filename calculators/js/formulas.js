70dfa49bbb3b64ad16a68e3e000b7a3956f3d75f
Tom Genoni <tom@atomeye.com>
2016-07-06 22:25:01 -0700

    Adding homework formulas.

--- calculators/js/formulas.js
+++ calculators/js/formulas.js
@@ -6,3 +6,20 @@
 // 3.	 Be sure to link up this file in your HTML doc
 /////////////////////////////////////////////////////////////////////
 
+function calcFahrenheitToCelcius(fahrenheit) {
+  // Takes fahrenheit value and returns celcius to 2 decimal points
+  var celcius = ((fahrenheit - 32) * (5/9)).toFixed(2);
+  return celcius;
+}
+
+function calcCircumference(radius) {
+  // Takes radius value and returns circumference
+  var circumference = 2 * Math.PI * radius;
+  return circumference;
+}
+
+function calcLongestSide(a, b) {
+  // Takes two shortest sides of a triangle and returns longest side
+  var hypotenuse = Math.sqrt(a*a + b*b);
+  return hypotenuse;
+}
