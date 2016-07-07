b48a3b77764574de9f923004ce3982cb05244e11
Tom Genoni <tom@atomeye.com>
2016-07-06 21:28:18 -0700

    Adding homework.

---
+++ functions/js/functions.js
@@ -0,0 +1,24 @@
+function subtract(x, y) {
+  var value = x - y;
+  return value;
+}
+
+function add(x, y) {
+  var value = x + y;
+  return value;
+}
+
+function multiply(x, y) {
+  var value = x * y;
+  return value;
+}
+
+function divide(x, y) {
+  var value = x / y;
+  return value;
+}
+
+function remainder(x, y) {
+  var value = x % y;
+  return value;
+}
