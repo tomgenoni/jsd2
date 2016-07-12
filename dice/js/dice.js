1db587aa468bde95a4c6db23ec5747541ad07a88
Tom Genoni <tom@atomeye.com>
2016-07-11 20:57:16 -0700

    Adding dice.

---
+++ dice/js/dice.js
@@ -0,0 +1,46 @@
+// Structure
+// ------------------------------------------
+var rollButton = document.querySelector('.roll-button');
+var firstDie   = document.querySelector('#first-die');
+var secondDie  = document.querySelector('#second-die');
+
+
+// Events
+// ------------------------------------------
+rollButton.addEventListener('click', rollDice);
+
+
+// Event Listeners
+// ------------------------------------------
+function randomNum(min, max) {
+  var num = (Math.random() * (max - min) + min).toFixed();
+  return num;
+}
+
+function rollDice() {
+  var prefix = 'dice-';
+  var random1 = randomNum(1,6);
+  var random2 = randomNum(1,6);
+  
+  firstDie.className = prefix + random1;
+  secondDie.className = prefix + random2;
+}
+
+/*
+Creating a function named 'rollDice' where every time the user hits the "Roll Dice" button, the
+screen randomly updates the two dice. Use the html and css code included
+in the starter code folder to get started.
+
+1) Write down pseudocode for the following program.
+
+2) Follow the steps below to write your code.
+* generate a random number between 1 - 6 and store to a variable, named 'random1'
+* generate a random number between 1 - 6 and store to a variable, named 'random2'
+* combine the string 'dice-' and random1 to form the random class for the first die, firstClass
+* combine the string 'dice-' and random2 to form the random class for the second die, secondClass
+* update the class of firstDie, using assignment, like firstDie.className =
+* update the class of secondDie, using assignment, like secondDie.className =
+
+3) Check to see if the Dice Roll has been hit, if it has run the rollDice function.
+
+*/
