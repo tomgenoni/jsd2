53b8121be7a991f2b5fa5145fbd30f764129a13d
Tom Genoni <tom@atomeye.com>
2016-08-13 18:18:00 -0700

    Adding weather template.

--- homework/weather/js/weather.js
+++ homework/weather/js/weather.js
@@ -1,5 +1,9 @@
-var form = document.querySelector('#zip');
-var body = document.querySelector('body');
+var form       = document.querySelector('#zip');
+var body       = document.querySelector('body');
+var results    = document.querySelector('.data');
+
+var hdbWeather = document.querySelector('#hlb-weather');
+
 
 form.addEventListener('submit', fetchWeather);
 
@@ -9,8 +13,16 @@ function fetchWeather(e) {
   showWeather();
 }
 
-
-function showWeather(data) {
-  body.classList.add('active');
+function showWeather() {
+  var weather = data.forecast.simpleforecast.forecastday;
   
+  var template = Handlebars.compile(hdbWeather.textContent);
+  var html = template(weather);
+  
+  results.innerHTML = html;
+  
+  setTimeout(function(){
+    body.classList.add('active');
+  },1)
+
 }
